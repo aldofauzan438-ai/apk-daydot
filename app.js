ccbc68fb5c3467a70432e8f4f3a2f8efa0a7e043
Aldo Fauzan <aldofauzan438@gmail.com>
2025-12-29 20:23:11 +0700

    update

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const {createApp}=Vue;
+const { createApp } = Vue;
+
 createApp({
-data(){
-return{
-menus:[
-'Food','Syrup',
-'Powder','Beans',
-'Inv Adjustment','Return Order',
-'HQ Order','Records'
-]
-}
-}
+  data() {
+    return {
+      buatBaru: 0,
+      template: 1,
+      expiredItems: [
+        "Food",
+        "Syrup",
+        "Powder",
+        "Beans",
+        "Inv Adjustment",
+        "Return Order",
+        "HQ Order",
+        "Records"
+      ]
+    };
+  },
+  methods: {
+    goTo(page) {
+      window.location.href = page;
+    }
+  }
 }).mount('#app');
