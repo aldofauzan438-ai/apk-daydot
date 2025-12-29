1205ff26246be204b6cd0bd5fd11e7ceba35faaf
Aldo Fauzan <aldofauzan438@gmail.com>
2025-12-29 21:48:35 +0700

    update

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      templates: [
+        "Template Harian TJ HI",
+        "Template Mingguan",
+        "Template Harian All Syrup & Food"
+    
+      ],
       expiredItems: [
         "Food",
         "Syrup",
         "Powder",
         "Beans",
-        "Inv Adjustment",
-        "Return Order",
-        "HQ Order",
-        "Records"
+        
       ]
     };
   },
@@ -20,7 +23,13 @@ createApp({
       window.location.href = "template.html";
     },
     goExpired() {
-      window.location.href = "detail.html";
+      window.location.href = "buat-baru.html";
+    },
+    openTemplate(name) {
+      window.location.href = `detail.html?template=${name}`;
+    },
+    openExpired(item) {
+      window.location.href = `detail.html?item=${item}`;
     }
   }
 }).mount("#app");
